a568f06b57557481a04f15f1fa9abfb67c2dcad5
Khanh Trang <trangkhanhx11@gmail.com>
2025-10-21 14:38:57 +0700

    Cập nhật giao diện Phụ huynh và controller mới

---
+++ KLTN2025/Controllers/PhuHuynhController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KLTN2025.Controllers
+{
+    public class PhuHuynhController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /PhuHuynh/PhuHuynhTrangChu
+        public IActionResult PhuHuynhTrangChu()
+        {
+            return View();
+        }
+        public IActionResult TimGiaSu()
+        {
+            return View();
+        }
+        public IActionResult ChiTietGiaSu()
+        {
+            return View();
+        }
+        public IActionResult QuanLyLopHoc()
+        {
+            return View();
+        }
+    }
+}
